b625d6a29723aa5a77d5bfdad37c9fdd6c6e00c4
Rainer Schuetze <r.sagitario@gmx.de>
2013-02-25 21:56:14 +0100

    add coff lib dependencies

--- src/rt/monitor_.d
+++ src/rt/monitor_.d
@@ -44,7 +44,14 @@ private
     version( Windows )
     {
         version (CRuntime_DigitalMars)
+        {
             pragma(lib, "snn.lib");
+        }
+        else version (CRuntime_Microsoft)
+        {
+            pragma(lib, "libcmt.lib");
+            pragma(lib, "oldnames.lib");
+        }
         import core.sys.windows.windows;
 
         struct Monitor
